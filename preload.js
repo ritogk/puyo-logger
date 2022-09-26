423468033a5b9eb7b2567e03e58f9754d7c65bf9
r.ito <homing0321r4cfw@yahoo.co.jp>
2022-09-26 14:10:43 +0900

    syokai komitto

---
+++ preload.js
@@ -0,0 +1,48 @@
+// preload.js
+
+// All the Node.js APIs are available in the preload process.
+// Chrome �g���@�\�Ɠ����T���h�{�b�N�X�������Ă��܂��B
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+
+// プリロードスクリプト内。
+const { ipcRenderer } = require("electron")
+
+ipcRenderer.on("SET_SOURCE", async (event, sourceId) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: "desktop",
+          chromeMediaSourceId: sourceId,
+          minWidth: 1280,
+          maxWidth: 1280,
+          minHeight: 720,
+          maxHeight: 720,
+        },
+      },
+    })
+    handleStream(stream)
+  } catch (e) {
+    handleError(e)
+  }
+})
+
+function handleStream(stream) {
+  const video = document.querySelector("video")
+  video.srcObject = stream
+  video.onloadedmetadata = (e) => video.play()
+}
+
+function handleError(e) {
+  console.log(e)
+}
